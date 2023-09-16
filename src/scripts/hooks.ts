625dd89d0f418016f3979be5448baf9d0b95c765
MemelyPepeartly <memelypepeartly@gmail.com>
2023-09-16 00:59:28 -0600

    logging

--- src/scripts/hooks.ts
+++ src/scripts/hooks.ts
@@ -1,6 +1,12 @@
 import { ActorPF2e } from "@actor";
 import { ActorSheetPF2e } from "@actor/sheet/base";
+import { logInfo } from "./utils";
 
+/**
+ * Hook that opens when the actor sheet is rendered
+ * @param sheet 
+ * @param $html 
+ */
 export function renderActorSheetHook(sheet: ActorSheetPF2e<ActorPF2e>, $html: JQuery) {
-    console.log("renderActorSheetHook called");
+    logInfo("renderActorSheetHook called", sheet, $html);
 }
