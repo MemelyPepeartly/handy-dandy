1c179e760e3feee1ce30797f41158de5e15eea64
MemelyPepeartly <memelypepeartly@gmail.com>
2023-10-13 00:21:18 -0600

    simplifying

--- src/scripts/helpers/ai-helper.ts
+++ src/scripts/helpers/ai-helper.ts
@@ -1,6 +1,4 @@
-import { AIOutput } from '../ai/models';
-
-export function processAIInput(input: any): AIOutput {
+export function processAIInput(input: any) {
   // Perform AI operations and return the result.
   console.log("Input data to Handy Dandy:", input);
   return null;
