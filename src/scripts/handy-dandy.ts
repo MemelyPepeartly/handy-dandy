bae85126bc11fd3e46f16a58764cd0d41418183c
MemelyPepeartly <memelypepeartly@gmail.com>
2023-10-15 17:37:47 -0600

    updated classes and parameters

--- src/scripts/handy-dandy.ts
+++ src/scripts/handy-dandy.ts
@@ -1,47 +1,47 @@
+
+import OpenAI, { ClientOptions } from "openai";
 import { MODULEID } from "./const";
+import { logInfo } from "./utils";
 
-const foundryGame: Game = game as Game;
+interface HandyDandyOptions extends FormApplicationOptions {
+    someCustomOption?: number;
+}
 
-export class HandyDandy extends Application {
-    settings: any;
-    constructor(options = {})
-    {
-        super(options);
+export class HandyDandy extends FormApplication<HandyDandyOptions> {
+    engine: OpenAIEngine | undefined;
 
-        this.settings = foundryGame.settings.get(MODULEID, "GPTApiKey");
-
+    protected _updateObject(event: Event, formData?: object): Promise<unknown> {
+        throw new Error("Method not implemented.");
     }
-    static override get defaultOptions(): ApplicationOptions {
-        return {
-            ...super.defaultOptions,
+
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
             id: "handy-dandy-app",
-            classes: [],
+            title: "Handy Dandy Application",
             template: "modules/handy-dandy/templates/handy-dandy.hbs",
-            resizable: true,
             dragDrop: [{ dragSelector: "ul.item-list > li.item" }],
-            tabs: []
-        }
+            tabs: [],
+            width: 500,
+            height: 500,
+            resizable: true
+        });
     }
 
-    initHandyDandy() {
-
-    }
-
-    submitForm(event: Event) {
-
-    }
-
-    override activateListeners($html: JQuery<HTMLElement>): void {
+    activateListeners($html: JQuery<HTMLElement>) {
         super.activateListeners($html);
+
+        $html.on('submit', 'form', (e) => {
+            e.preventDefault();
+        });
     
-        // Get the button and form within this application's rendered HTML using jQuery
         const $button = $html.find("#generateButton");
-        const $form = $html.find("#handy-dandy-form");
-    
         $button.on('click', (e) => {
-            console.log("Button clicked!");
+            // get form values and create an object to pass to the processAIInput function
+            const $form = $html.find("#handy-dandy-form");
             const serializedData = $form.serialize();
-            this.processAIInput(serializedData);
+            
+            const formValues = FormInput.deserialize(serializedData);
+            this.processAIInput(formValues);
         });
     }
 
@@ -50,6 +50,52 @@ export class HandyDandy extends Application {
         console.log("Input data to Handy Dandy:", input);
         return null;
       }
-      
-    
+}
+
+export class FormInput {
+    prompt: string;
+    context: string;
+    parameters: {
+        temperature: number;
+        maxLength: number;
+    };
+
+    constructor(prompt: string, context: string, temperature: number, maxLength: number) {
+        this.prompt = prompt;
+        this.context = context;
+        this.parameters = {
+            temperature: temperature,
+            maxLength: maxLength
+        };
+    }
+
+    // Convert instance to a serialized string
+    serialize(): string {
+        const serializedParameters = `parameters[temperature]=${this.parameters.temperature}&parameters[maxLength]=${this.parameters.maxLength}`;
+        return `prompt=${encodeURIComponent(this.prompt)}&context=${encodeURIComponent(this.context)}&${serializedParameters}`;
+    }
+
+    // Create an instance from a serialized string
+    static deserialize(input: string): FormInput {
+        const params = new URLSearchParams(input);
+        const prompt = params.get('prompt') || '';
+        const context = params.get('context') || '';
+        const temperature = parseFloat(params.get('parameters[temperature]') || '0');
+        const maxLength = parseInt(params.get('parameters[maxLength]') || '0');
+        return new FormInput(prompt, context, temperature, maxLength);
+    }
+}
+
+export class OpenAIEngine {
+    formInput?: FormInput;
+    options?: ClientOptions;
+
+    constructor(apiKey?: string) {
+        this.options = {
+            apiKey: apiKey
+        };
+    } 
+    createCompletionRequest(): any {
+        console.log("Creating completion request");
+    }
 }
