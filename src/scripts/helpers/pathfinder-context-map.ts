11aeeb7e21a95cf03f86226869e74494dadec446
MemelyPepeartly <memelypepeartly@gmail.com>
2024-01-21 00:10:06 -0700

    type map

---
+++ src/scripts/helpers/pathfinder-context-map.ts
@@ -0,0 +1,10 @@
+const pathfinderContextMap = {
+    "feat": "Create a detailed and balanced description of a Pathfinder 2e feat based on the following user provided prompt for it...",
+    "item": "Describe a detailed and balanced Pathfinder 2e item with its properties based on the following user provided prompt for it...",
+    "action": "Generate a detailed and balanced description of a Pathfinder 2e action based on the following user provided prompt for it..",
+    "spell": "Generate a detailed and balanced description of a Pathfinder 2e spell based on the following user provided prompt for it...",
+    "monster": "Generate a detailed and balanced description of a Pathfinder 2e monster based on the following user provided prompt for it...",
+    "npc": "Generate a detailed and balanced description of a Pathfinder 2e NPC based on the following user provided prompt for it..."
+};
+
+export default pathfinderContextMap;
