9ea84e58393344b791a77d6197779728231e5bbe
MemelyPepeartly <memelypepeartly@gmail.com>
2024-01-20 22:09:08 -0700

    removed old implimentation

--- src/scripts/module/api.ts
+++
@@ -1,9 +0,0 @@
-import CONSTANTS from "./constants";
-
-const API = {
-  get DEFAULT_PROPERTIES(): any {
-    return game.settings.get(CONSTANTS.MODULE_NAME, "GPTApiKey");
-  }
-};
-
-export default API;
