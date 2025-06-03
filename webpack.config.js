31199e4b02da30c9b1ca4cadd01eaed3769caca8
MemelyPepeartly <memelypepeartly@gmail.com>
2025-06-02 22:06:49 -0400

    feat: add additional styles directory to webpack configuration for asset management

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ export default {
         {
           from: "src/templates",
           to: "templates"
+        },
+        {
+          from: "src/styles",
+          to: "styles"
         }
       ]
     }),
